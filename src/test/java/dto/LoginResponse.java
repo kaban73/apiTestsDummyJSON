67536e87c92bdd967b46f163b1bd6af8c4e1779f
Ivan Kabanov <i.borkoff@yandex.ru>
2026-09-07 12:52:24 +0400

    написал тест

--- src/test/java/dto/LoginResponse.java
+++ src/test/java/dto/LoginResponse.java
@@ -1,5 +1,8 @@
 package dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record LoginResponse(
         long id,
         String username
---
+++ src/test/java/tests/AuthTests.java
@@ -0,0 +1,31 @@
+package tests;
+
+import clients.AuthClient;
+import dto.LoginRequest;
+import dto.LoginResponse;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class AuthTests {
+
+    @Test
+    public void authLoginTest() {
+        LoginRequest request = new LoginRequest(
+                "emilys",
+                "emilyspass"
+        );
+        AuthClient authClient = new AuthClient();
+
+        Response response = authClient.authLogin(request);
+
+        response.then().statusCode(200);
+
+        LoginResponse loginResponse = response.as(LoginResponse.class);
+
+        assertTrue(loginResponse.id() > 0);
+        assertEquals(loginResponse.username(), request.username());
+    }
+
+}
