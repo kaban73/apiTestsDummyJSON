872cb6dbe70aedcdd20a5a4cee68666eed8b4c60
Ivan Kabanov <i.borkoff@yandex.ru>
2026-09-07 12:10:23 +0400

    добавил спекФактори

---
+++ src/test/java/specs/RequestSpecFactory.java
@@ -0,0 +1,28 @@
+package specs;
+
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
+public class RequestSpecFactory {
+
+    private RequestSpecFactory() {
+
+    }
+
+    public static RequestSpecification baseSpecs() {
+        return given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .baseUri("https://dummyjson.com/");
+    }
+
+    public static RequestSpecification authSpec(String token) {
+        return given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .baseUri("https://dummyjson.com/")
+                .param("Bearer: " + token);
+    }
+}
